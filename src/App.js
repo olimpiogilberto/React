c52f209dc10ccea8e471ed7ec34f3e5eaca1d482
Gilberto Olimpio dos Santos <c129427@corp.caixa.gov.br>
2020-05-23 15:25:39 -0300

    correção da lista
    A correção do erro no props foi na verdade  totalmente no App.js

--- src/App.js
+++ src/App.js
@@ -4,52 +4,47 @@ import './App.css';
 import Tabela from './Tabela';
 
 class App extends Component {
+  
   state = {
-    autores:  [
-    {
-      nome: 'Paulo',
-      livro: 'React',
-      preco: '1000'
-    },
-    {
-      nome: 'Daniel',
-      livro: 'Java',
-      preco: '99'
-    },
-    {
-      nome: 'Marcos',
-      livro: 'Design',
-      preco: '150'
-    },
-    {
-      nome: 'Bruno',
-      livro: 'DevOps',
-      preco: '100'
-    },
-    {
-      nome: 'Giba',
-      livro: 'testeGiba',
-      preco: '100,00'
-    }
-  ],
-}
-
-removerAutor = index => {
-
-    const { autores } = this.state;
-
-    this.setState({
-          autores : autores.filter((autor, posAtual) => {
-                    return posAtual !== index;
-          }),
-      })
-}
+    autores :  [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      }
+    ],
+  }
     
+removeAutor = index => {
+
+  const { autores } = this.state;
+  
+  this.setState({
+    autores : autores.filter((autor, posAtual) => {
+      return posAtual !== index;
+    }),
+  })
+  
+}
+
   render() {
     return (
-      
-      <Tabela autores={this.state.autores} removerAutor = {this.removerAutor} />  
-      
+      <Tabela autores={this.state.autores} removeAutor = { this.removeAutor }/>  
     );
   }
 }
